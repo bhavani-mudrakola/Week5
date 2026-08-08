efb536462983e5456338e3b14d5aad3f76f768d4
bhavani-mudrakola <bhavani.mudrakola@gmail.com>
2026-08-08 12:16:11 +0530

    my project commit

--- TestFactorial.java
+++ TestFactorial.java
@@ -1,11 +1,8 @@
 public class TestFactorial {
 
     public static void main(String[] args) {
-
         int n = 5;
-
         int result = Factorial.calculate(n);
-
         System.out.println("Factorial of " + n + " = " + result);
     }
 }
